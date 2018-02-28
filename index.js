d2a934a8a62c442d3b4a68f4c07936f1fb075878
Celeste Gerard <celeste.gerard@gmail.com>
2018-02-28 21:59:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push("name");
+  return katzDeliLine;
+  }
+  
+  function nowServing
+  
+  
+  function currentLine(KatzDeliLine) {
+    return "The line is currently:"${number}"."${name}","${number}"."${name}""
+  }
